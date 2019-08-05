7322089badd2c7dc4ed6b3cd507cf24f4ac39a32
Hira Ibrahim <hiraibrahim197@gmail.com>
2019-08-05 14:24:53 +0500

    -Settings Activity commit and push

--- app/src/main/java/com/example/ecommerce/HomeActivity.java
+++ app/src/main/java/com/example/ecommerce/HomeActivity.java
@@ -171,7 +171,7 @@ public class HomeActivity extends AppCompatActivity
         else if (id == R.id.nav_settings)
         {
             Intent intent = new Intent(HomeActivity.this, Settings_Activity.class);
-            startActivity(intent);
+            startActivity(intent);  
         }
         else if (id == R.id.nav_logout)
         {
